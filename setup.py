2e38a7976cecac34a3a1f9d76f035cf8abc45e36
Dan Braghis <dan.braghis@torchbox.com>
2019-08-21 16:21:49 +0100

    Explicit utf-8 encoding for long description
    looks like pip install fails on some systems with `UnicodeDecodeError: 'ascii' codec can't decode byte 0xf0 in position 6282: ordinal not in range(128)`

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from wagtail_headless_preview import __version__
 
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, "README.md")) as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 
