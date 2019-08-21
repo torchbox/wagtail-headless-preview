6996508f2c8a2f99f2b045b19fdea0ece7eddd37
Dan Braghis <dan.braghis@torchbox.com>
2019-08-21 16:22:00 +0100

    More blackify

--- runtests.py
+++ runtests.py
@@ -6,22 +6,28 @@ import warnings
 
 from django.core.management import execute_from_command_line
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'wagtail_headless_preview.tests.settings'
+os.environ["DJANGO_SETTINGS_MODULE"] = "wagtail_headless_preview.tests.settings"
 
 
 def runtests():
     # Don't ignore DeprecationWarnings
-    only_wagtail_headless_preview = r'^wagtail_headless_preview(\.|$)'
-    warnings.filterwarnings('default', category=DeprecationWarning, module=only_wagtail_headless_preview)
-    warnings.filterwarnings('default', category=PendingDeprecationWarning, module=only_wagtail_headless_preview)
+    only_wagtail_headless_preview = r"^wagtail_headless_preview(\.|$)"
+    warnings.filterwarnings(
+        "default", category=DeprecationWarning, module=only_wagtail_headless_preview
+    )
+    warnings.filterwarnings(
+        "default",
+        category=PendingDeprecationWarning,
+        module=only_wagtail_headless_preview,
+    )
 
     args = sys.argv[1:]
-    argv = sys.argv[:1] + ['test'] + args
+    argv = sys.argv[:1] + ["test"] + args
     try:
         execute_from_command_line(argv)
     finally:
         pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     runtests()
