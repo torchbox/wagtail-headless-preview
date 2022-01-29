bc43cf54fda9fc692d02c99582fd894a32ececf1
Dan Braghis <dan.braghis@torchbox.com>
2022-01-29 00:01:48 +0000

    Add interactive mode to tox configuration

---
+++ manage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+
+if __name__ == "__main__":
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", "wagtail_headless_preview.tests.settings"
+    )
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
