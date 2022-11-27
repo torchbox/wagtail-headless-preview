c9ee9d9e7d6547dc48ca35943823526613916d64
Dan Braghis <dan.braghis@torchbox.com>
2022-11-27 11:33:10 +0000

    Bump deprecation warnings versions

--- src/wagtail_headless_preview/deprecation.py
+++ src/wagtail_headless_preview/deprecation.py
@@ -1,10 +1,10 @@
-class RemovedInWagtailHeadlessPreview050Warning(DeprecationWarning):
+class RemovedInWagtailHeadlessPreview060Warning(DeprecationWarning):
     pass
 
 
-class RemovedInWagtailHeadlessPreview060Warning(PendingDeprecationWarning):
+class RemovedInWagtailHeadlessPreview070Warning(PendingDeprecationWarning):
     pass
 
 
-removed_in_next_version_warning = RemovedInWagtailHeadlessPreview050Warning
-pending_deprecation_warning = RemovedInWagtailHeadlessPreview060Warning
+removed_in_next_version_warning = RemovedInWagtailHeadlessPreview060Warning
+pending_deprecation_warning = RemovedInWagtailHeadlessPreview070Warning
