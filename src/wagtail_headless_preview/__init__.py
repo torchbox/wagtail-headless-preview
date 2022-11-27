b1c573fafb22585d8cf5ef06e8d62e616f3970eb
Dan Braghis <dan.braghis@torchbox.com>
2022-11-27 11:32:49 +0000

    Drop version file and Django 3.2 conditional logic

--- src/wagtail_headless_preview/__init__.py
+++ src/wagtail_headless_preview/__init__.py
@@ -1,13 +1,5 @@
-from django import VERSION as DJANGO_VERSION
-
-from .version import VERSION, __version__  # noqa
-
-
-if DJANGO_VERSION >= (3, 2):
-    # The declaration is only needed for older Django versions
-    pass
-else:
-    default_app_config = "wagtail_headless_preview.apps.WagtailHeadlessPreviewConfig"
+VERSION = (0, 4, 0)
+__version__ = ".".join(map(str, VERSION))
 
 
 def setup():
